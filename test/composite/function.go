7be00e182a5ce42986170098fe2068eb45df2918
leson <i.leson@163.com>
2022-10-30 00:16:34 +0800

    append advance closure

--- test/composite/function.go
+++ test/composite/function.go
@@ -1,6 +1,9 @@
 package composite
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func TestFunc() {
 	for i := 0; i < 10; i++ {
@@ -11,6 +14,9 @@ func TestFunc() {
 	testAnonymousFunc()
 	testSelfInvocation()
 	testFuncCaculateInternal()
+	testClosure()
+	testClosure1()
+	testClosure2()
 }
 
 func fibonacci(n int) int {
@@ -54,4 +60,62 @@ func testFuncCaculateInternal() {
 		return name + age
 	}()
 	fmt.Printf("s1: %v\n", s1)
+}
+
+func testClosure() {
+	f := add()
+	fmt.Printf("f: %v\n", f(10))
+	fmt.Printf("f: %v\n", f(10))
+	fmt.Println("======================")
+	f1 := add()
+	fmt.Printf("f1 return: %v\n", f1(100))
+	fmt.Printf("f1 return: %v\n", f1(100))
+}
+
+func add() func(int) int {
+	var x int
+	return func(y int) int {
+		x += y
+		return x
+	}
+}
+
+func testClosure1() {
+	jpgFunc := makeSuffixFunc(".jpg")
+	txtFunc := makeSuffixFunc(".txt")
+	fmt.Printf("jpgFunc: %v\n", jpgFunc("hello.jpg"))
+	fmt.Printf("txtFunc: %v\n", txtFunc("world"))
+}
+
+func makeSuffixFunc(suffix string) func(string) string {
+	return func(name string) string {
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		} else {
+			return name
+		}
+
+	}
+}
+
+func testClosure2() {
+	add, sub := calc(50)
+	fmt.Printf("add 10: %v\n", add(10))
+	fmt.Printf("sub 20: %v\n", sub(20))
+	fmt.Printf("add 30: %v\n", add(30))
+	fmt.Printf("sub 100: %v\n", sub(100))
+}
+
+func calc(t int) (func(int) int, func(int) int) {
+	add := func(i int) int {
+		t += i
+		return t
+	}
+
+	sub := func(i int) int {
+		t -= i
+		return t
+	}
+
+	return add, sub
 }
